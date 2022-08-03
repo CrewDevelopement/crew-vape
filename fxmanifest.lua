c14b286b26284e9d367bfb6a5e211e0e4b13ea1e
Crew <97802269+CrewDevelopement@users.noreply.github.com>
2022-08-02 22:14:10 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Crew-Vape'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua',
+}
+client_script 'client/server.lua'
+server_script 'server/server.lua'
